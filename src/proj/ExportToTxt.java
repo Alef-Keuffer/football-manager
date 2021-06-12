d55e8a8640eadd20444ca2dfacf0589f3c55a1f6
fmbicalho <fernando_bicalho_skate@msn.com>
2021-06-12 11:14:26 +0100

    Update ExportToTxt.java

--- src/proj/ExportToTxt.java
+++ src/proj/ExportToTxt.java
@@ -23,13 +23,13 @@ public class ExportToTxt {
         
     private static void logToTxt(){  
         try {
-        String fileName = new SimpleDateFormat("logs.txt'").format(new Date());
+        String fileName = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss'.txt'").format(new Date());
         BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
         
         //Deve surgir no ficheiro:
         //Equipa: Nome equipa
         //Posição: Nome + atributos
-        //
+        //....
         //Jogo: Equipa x, equipa y, resultado(x,y), data, num camisolas equipa x, subs equipa x, num camisolas equipa y, subs equipa y
         
         writer.write("Something" + variable.getText() + "\n");
